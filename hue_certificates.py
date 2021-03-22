7b2ca4748318c5c87deb9d3851afae110e5095cc
wrmacdonald <64927843+wrmacdonald@users.noreply.github.com>
2021-03-22 12:25:23 -0700

    Add files via upload

---
+++ hue_certificates.py
@@ -0,0 +1,13 @@
+# hue certificates
+
+ip = "xxx.xxx.x.x"
+username = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+# light to change
+office_light = "light_name"
+
+# to communicate with Hue lights
+# hue = requests.get("https://<ip>/debug/clip.html")
+# hue = requests.get("http://<ip>/api/<username>/lights/<office_light>")
+# hue_office_on = requests.put("http://<ip>/api/<username>/lights/<office_light>/state", '{"on":true}')
+# hue_office_off = requests.put("http://<ip>/api/<username>/lights/<office_light>/state", '{"on":false}')
